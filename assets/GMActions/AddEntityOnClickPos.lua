fe789a486305073613a64641b0355c1d685bd353
Elliot Drees <elliot@trellick.net>
2025-07-25 13:28:34 -0400

    Example spawning entities

--- assets/GMActions/AddEntityOnClickPos.lua
+++ assets/GMActions/AddEntityOnClickPos.lua
@@ -1,8 +1,24 @@
 local Template = require("./../scripts/Template")
 local waarg = require("./../scripts/FirstTemplates")
 
+local is_first = true;
+
 function on_click(x, y)
-    Template.spawnTemplate(waarg.test1,x,y)
-    print("Entity".. waarg.test1.name .." at position: " .. x .. ", " .. y)
+    if is_first then
+        Template.spawnTemplate(waarg.sensor,x,y)
+        print("Entity ".. waarg.sensor.name .." at position: " .. x .. ", " .. y)
+        is_first = false
+    else
+        Template.spawnTemplate(waarg.sensor_trace,x,y)
+        print("Entity ".. waarg.sensor_trace.name .." at position: " .. x .. ", " .. y)
+    end
 end
 
+
+function on_script_reloaded(value)
+    is_first = value
+end
+
+function on_script_unloaded()
+    return is_first
+end
